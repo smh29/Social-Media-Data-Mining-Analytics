9b988bc4b2f2dfa2a09bacfa1929d46addfd029f
Scott <smh29@sfu.ca>
2021-03-13 11:44:45 -0800

    Add files via upload

---
+++ data/download_all.sh
@@ -0,0 +1,16 @@
+# Download all data data sources used in this book.
+
+# Wikipedia revision metadata.
+src/chapter1/get_wikipedia_data.sh
+
+# LiveJournal social network data
+src/chapter2/get_livejournal_data.sh
+
+# StackExchange articles.
+src/chapter4/get_stackexchange_data.sh
+
+# Amazon Finefoods dataset.
+src/chapter5/dataload/get_finefoods_data.sh
+
+# MovieLens dataset for movie ratings.
+src/chapter6/get_movielens_data.sh
